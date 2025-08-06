5e498b8db9cad120e860481b3c5dffd2cfe93f1b
Jazmine T. <202853938+ceocleaning@users.noreply.github.com>
2025-08-06 10:47:43 +0500

    Update views.py

--- invoice/views.py
+++ invoice/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.conf import settings
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse, JsonResponse, Http404
 from .models import Invoice, Payment
 from bookings.models import Booking
 import datetime
