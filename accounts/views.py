663169a083427474ec8136342035ed2058e88712
Jazmine T. <202853938+ceocleaning@users.noreply.github.com>
2025-08-26 17:01:26 +0500

    Update views.py

--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404, HttpResponse
 from django.contrib.auth import login, authenticate, logout, update_session_auth_hash
 from django.contrib import messages
 from django.views.decorators.http import require_http_methods
+from django.http import JsonResponse, HttpResponse
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import PasswordChangeForm
