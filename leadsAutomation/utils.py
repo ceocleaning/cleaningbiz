069e35f663596c8915aca21e12ffe392c451be14
kashif56 <8bpcoins4u@gmail.com>
2025-09-03 02:44:03 +0500

    feat: add attachment support to email sending utility function

--- leadsAutomation/utils.py
+++ leadsAutomation/utils.py
@@ -3,7 +3,7 @@ from django.conf import settings
 
 EMAILIT_API_KEY = settings.EMAILIT_API_KEY
 
-def send_email(from_email, to_email, subject, reply_to=None, html_body='', text_content=''):
+def send_email(from_email, to_email, subject, reply_to=None, html_body='', text_content='', attachments=None):
     """
     Send an email using the EmailIt API.
 
@@ -14,29 +14,35 @@ def send_email(from_email, to_email, subject, reply_to=None, html_body='', text_
     - reply_to (str, optional): The reply-to email address.
     - html_body (str, optional): The HTML content of the email.
     - text_content (str, optional): The plain text content of the email.
+    - attachments (list of dict, optional): Attachments [{'filename': '...', 'content': '...', 'content_type': '...'}].
     """
     url = 'https://api.emailit.com/v1/emails'
-    headers = {
+    req_headers = {
         'Authorization': f'Bearer {EMAILIT_API_KEY}',
         'Content-Type': 'application/json'
     }
+
     data = {
         'from': from_email,
         'to': to_email,
-        'reply_to': reply_to,
         'subject': subject,
         'html': html_body,
         'text': text_content
     }
 
-    response = requests.post(url, json=data, headers=headers)
-    
+    if reply_to:
+        data['reply_to'] = reply_to
+    if attachments:
+        data['attachments'] = attachments
+
+
+    response = requests.post(url, json=data, headers=req_headers)
+
     try:
         return response.json()
     except ValueError:
         print(response.text)
         print(response.status_code)
-        # Handle the case where the response is not valid JSON
         return {
             'success': False,
             'error': f'Invalid JSON response. Status code: {response.status_code}',
